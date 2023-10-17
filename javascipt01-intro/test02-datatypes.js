fccbba6f3b1a90f132cca064d57beb98e0125386
Ganesh3438 <ganesh2638@gmail.com>
2023-10-17 16:40:08 +0530

    Added test02-datatypes.js file

---
+++ javascipt01-intro/test02-datatypes.js
@@ -0,0 +1,16 @@
+// Data types in JS:
+// We dont need to specify the data types in javascript either we use var, let and const
+
+// There are two types of data types:
+// 1. primitive data type 2. Reference data types
+
+// Primitive data types are nothing but Number, String, Boolean, Undefined and Null
+// Reference data types are Objects, Arrays and functions
+
+// What is Arrays?
+// Array is a data structure which holds similar kind of data in indexed order
+
+// Declaration of array in JS:
+let arr = [1, 2, 3, 4, 5]
+console.log(arr);
+
