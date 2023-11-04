b0bf29cdb943d84490cc02d29f2aa711feca20bd
Ganesh3438 <ganesh2638@gmail.com>
2023-11-04 19:49:51 +0530

    Made changes in TC02-promise01.js

--- Asynchronous-part2/TC02-promise01.js
+++ Asynchronous-part2/TC02-promise01.js
@@ -1,19 +1,19 @@
-// const fs = require("fs");
+const fs = require("fs");
 
-// let f1p = fs.promises.readFile("TC02-file1.txt", readFileCallBack)
-// let f2p = fs.promises.readFile("TC02-file1.txt", readFileCallBack)
-// let f3p = fs.promises.readFile("TC02-file1.txt", readFileCallBack)
+let f1p = fs.promises.readFile("TC02-file1.txt", readFileCallBack)
+let f2p = fs.promises.readFile("TC02-file2.txt", readFileCallBack)
+let f3p = fs.promises.readFile("TC02-file3.txt", readFileCallBack)
 
 
-// function readFileCallBack(data){
-//        console.log('This is the data ---> ' + data)
-// }
+function readFileCallBack(data){
+       console.log('This is the data ---> ' + data)
+}
 
-// function handleError(err){
-//     console.log('This is the data ---> ' + err)
-// }
+function handleError(err){
+    console.log('This is the data ---> ' + err)
+}
 
-// f1p.then(readFileCallBack)
-// f2p.then(readFileCallBack)
-// f3p.then(readFileCallBack)
+f1p.then(readFileCallBack)
+f2p.then(readFileCallBack)
+f3p.then(readFileCallBack)
 
