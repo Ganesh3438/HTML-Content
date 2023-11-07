dbde4ad310950e491aea01448d88847405faf891
Ganesh3438 <ganesh2638@gmail.com>
2023-11-07 07:56:06 +0530

    Added asyn and await concept

---
+++ Asynchronous-part3/TC02-Async.js
@@ -0,0 +1,63 @@
+function placeOrder(drink){
+     return new Promise(function(resolve, reject){
+        if(drink==='coffee'){
+            resolve('Order for coffee placed')
+        }else{
+            reject('Order cannot be placed')
+        }
+     })
+}
+
+function processYourOrder(orderPlaced){
+
+    return new Promise(function(resolve){
+         resolve(`${orderPlaced} and served`)
+    })
+
+}
+
+function generateBill(processedOrder){
+   return new Promise(function(resolve){
+         resolve(`${processedOrder} and Bill generated with 200Rs`)
+   })
+}
+
+// placeOrder('coffee').then(function(orderStatus){
+//     console.log(orderStatus)
+//     return orderStatus
+// }).then(function(orderStatus){
+// let orderIsProcessed = processYourOrder(orderStatus)
+// // console.log(orderIsProcessed)
+// return orderIsProcessed;
+// }).then(function(orderIsProcessed){
+//     console.log(orderIsProcessed)
+//     return orderIsProcessed
+// }).then(function(orderIsProcessed){
+//     let bill = generateBill(orderIsProcessed)
+//     return bill
+// }).then(function(bill){
+//     console.log(bill)
+// }).catch(function(error){
+//     console.log(error)
+// })
+
+async function serveOrder(){
+    
+    
+   try {
+    
+    let orderStatus = await placeOrder('coffee')
+    console.log(orderStatus)
+    let processOrder = await processYourOrder(orderStatus);
+    console.log(processOrder)
+    let bill = await generateBill(processOrder)
+    console.log(bill)
+
+   } catch (error) {
+     console.log(error)
+   }
+    
+    
+}
+
+serveOrder()
